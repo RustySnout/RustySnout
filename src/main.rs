a723919a5904ee0e18cb96b658a00e8bfc46e5eb
omarelfouly <omarelfouly@aucegypt.edu>
2024-04-06 20:05:37 +0200

    Refactors code

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,34 @@
-fn main() {
-    use sysinfo::Networks;
+use sysinfo::Networks;
 
+fn main() {
     // Read all interfaces
     let mut networks = Networks::new_with_refreshed_list();
-    for (interface_name, data) in &networks {
-        println!(
-            "{interface_name}: {} B (down) / {} B (up)",
-            data.total_received(),
-            data.total_transmitted()
-        );
-    }
+    print_interfaces_total(&networks);
     std::thread::sleep(std::time::Duration::from_secs(10));
 
     networks.refresh();
 
     println!("\nAfter 10 seconds:");
 
-    for(interface_name, data) in &networks {
+    print_interfaces(&networks);
+}
+
+fn print_interfaces_total(networks: &Networks) {
+    for (interface_name, data) in networks {
+        println!(
+            "{interface_name}: {} B (down) / {} B (up)",
+            data.total_received(),
+            data.total_transmitted()
+        );
+    }
+}
+
+fn print_interfaces(networks: &Networks) {
+    for (interface_name, data) in networks {
         println!(
             "{interface_name}: {} B (down) / {} B (up)",
             data.received(),
             data.transmitted()
-        )
+        );
     }
 }
