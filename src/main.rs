212f1587e17bce92fb80d3bba2fb7f2d8227aceb
Hussein <h.eazhary@gmail.com>
2024-04-06 22:01:00 +0200

    Adds Interface reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    use sysinfo::Networks;
+
+    // Read all interfaces
+    let mut networks = Networks::new_with_refreshed_list();
+    for (interface_name, data) in &networks {
+        println!(
+            "{interface_name}: {} B (down) / {} B (up)",
+            data.total_received(),
+            data.total_transmitted()
+        );
+    }
+    std::thread::sleep(std::time::Duration::from_secs(10));
+
+    networks.refresh();
+
+    println!("\nAfter 10 seconds:");
+
+    for(interface_name, data) in &networks {
+        println!(
+            "{interface_name}: {} B (down) / {} B (up)",
+            data.received(),
+            data.transmitted()
+        )
+    }
 }
