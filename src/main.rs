280ddb7f2745aac2451b7fbaf931d8494f5a1c53
omarelfouly <omarelfouly@aucegypt.edu>
2024-04-29 20:49:43 +0300

    Gets open Socket for linux

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,75 @@
 
 use pnet::datalink::{self /*NetworkInterface*/};
 use rusqlite::{params, Connection /* , Result*/};
+use std::fmt;
 use std::io;
+
+use std::collections::HashMap;
+
+use procfs::process::FDTarget;
+
+use std::net::IpAddr;
+
+#[derive(PartialEq, Hash, Eq, Clone, PartialOrd, Ord, Debug, Copy)]
+pub enum Protocol {
+    Tcp,
+    Udp,
+}
+
+impl Protocol {
+    #[allow(dead_code)]
+    pub fn from_str(string: &str) -> Option<Self> {
+        match string {
+            "TCP" => Some(Protocol::Tcp),
+            "UDP" => Some(Protocol::Udp),
+            _ => None,
+        }
+    }
+}
+
+impl fmt::Display for Protocol {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Protocol::Tcp => write!(f, "tcp"),
+            Protocol::Udp => write!(f, "udp"),
+        }
+    }
+}
+
+#[derive(PartialEq, Hash, Eq, Clone, PartialOrd, Ord, Copy)]
+pub struct LocalSocket {
+    pub ip: IpAddr,
+    pub port: u16,
+    pub protocol: Protocol,
+}
+
+impl fmt::Debug for LocalSocket {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let LocalSocket { ip, port, protocol } = self;
+        match ip {
+            IpAddr::V4(v4) => write!(f, "{protocol}://{v4}:{port}"),
+            IpAddr::V6(v6) => write!(f, "{protocol}://[{v6}]:{port}"),
+        }
+    }
+}
+
+pub struct OpenSockets {
+    sockets_to_procs: HashMap<LocalSocket, ProcessInfo>,
+}
+
+#[derive(Clone, Debug, Default, Hash, PartialEq, Eq)]
+pub struct ProcessInfo {
+    pub name: String,
+    pub pid: u32,
+}
+impl ProcessInfo {
+    pub fn new(name: &str, pid: u32) -> Self {
+        Self {
+            name: name.to_string(),
+            pid,
+        }
+    }
+}
 
 struct Process {
     id: String,
@@ -179,5 +247,70 @@ fn main() -> io::Result<()> {
         }
     }
 
+    // Get the open sockets
+    let open_sockets = get_open_sockets();
+
+    // Print the open sockets
+    for (socket, proc_info) in open_sockets.sockets_to_procs {
+        let ip = match socket.ip {
+            IpAddr::V4(ip) => ip.to_string(),
+            IpAddr::V6(ip) => ip.to_string(),
+        };
+        let port = socket.port;
+        let protocol = match socket.protocol {
+            Protocol::Tcp => "TCP",
+            Protocol::Udp => "UDP",
+        };
+        let process_name = proc_info.name;
+        let pid = proc_info.pid;
+
+        println!(
+            "Socket: {}:{} Protocol: {} Process: {} PID: {}",
+            ip, port, protocol, process_name, pid
+        );
+    }
+
     Ok(())
+}
+
+pub fn get_open_sockets() -> OpenSockets {
+    let mut open_sockets = HashMap::new();
+    let mut inode_to_proc = HashMap::new();
+
+    if let Ok(all_procs) = procfs::process::all_processes() {
+        for process in all_procs.filter_map(|res| res.ok()) {
+            let Ok(fds) = process.fd() else { continue };
+            let Ok(stat) = process.stat() else { continue };
+            let proc_name = stat.comm;
+            let proc_info = ProcessInfo::new(&proc_name, stat.pid as u32);
+            for fd in fds.filter_map(|res| res.ok()) {
+                if let FDTarget::Socket(inode) = fd.target {
+                    inode_to_proc.insert(inode, proc_info.clone());
+                }
+            }
+        }
+    }
+
+    macro_rules! insert_proto {
+        ($source: expr, $proto: expr) => {
+            let entries = $source.into_iter().filter_map(|res| res.ok()).flatten();
+            for entry in entries {
+                if let Some(proc_info) = inode_to_proc.get(&entry.inode) {
+                    let socket = LocalSocket {
+                        ip: entry.local_address.ip(),
+                        port: entry.local_address.port(),
+                        protocol: $proto,
+                    };
+                    open_sockets.insert(socket, proc_info.clone());
+                }
+            }
+        };
+    }
+
+    insert_proto!([procfs::net::tcp(), procfs::net::tcp6()], Protocol::Tcp);
+    insert_proto!([procfs::net::udp(), procfs::net::udp6()], Protocol::Udp);
+
+    OpenSockets {
+        sockets_to_procs: open_sockets,
+    }
 }
